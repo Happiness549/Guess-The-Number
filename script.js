4008c72fb9facc34c2b3f4cbb85d6269e899fb30
Happiness549 <mhlongohappiness880@gmail.com>
2025-06-06 18:45:25 +0200

    Add JavaScript functionality

--- script.js
+++ script.js
@@ -1,4 +1,55 @@
 'use strict';
-let colors = ['blue', 'green', 'purple'];
-function changeColor() {}
-console.log('Hello world!');
+
+let number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.number').textContent = number;
+
+function play() {
+  const guess = Number(document.querySelector('.guess').value);
+  // when there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+
+    // when player wins
+  } else if (guess === number) {
+    document.querySelector('.message').textContent = 'Correct Number';
+    document.querySelector('.number').textContent = number;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+    //when score is too high
+  } else if (guess !== number) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        guess > number ? 'Too high' : 'Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+}
+
+const checkElement = document.querySelector('.check');
+checkElement.addEventListener('click', play);
+
+function again() {
+  score = 20;
+  number = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing ..';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+}
+const againElement = document.querySelector('.again');
+againElement.addEventListener('click', again);
